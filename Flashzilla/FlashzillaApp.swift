0013fee74077511d4eae76fafefb5112443c71e0
Carlos Eduardo Witte <cewitte@icloud.com>
2025-07-23 10:17:37 -0300

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Carlos Eduardo Witte on 23/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
